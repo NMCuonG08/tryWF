1814e808ac0022944814a38b5e9d4875e4150551
Cuong <121237768+NMCuonG08@users.noreply.github.com>
2024-02-02 19:46:59 +0700

    Update Form1.Designer.cs change text

--- TestGit/Form1.Designer.cs
+++ TestGit/Form1.Designer.cs
@@ -39,7 +39,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Hi ace";
+            this.label1.Text = "Duma cuoc doi";
             // 
             // button1
             // 
